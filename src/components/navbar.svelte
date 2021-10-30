9bc9d4681423720162810556ca492b6910b8deca
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2021-10-30 22:09:08 +0300

    Fixed navbar icons in Safari

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -78,7 +78,7 @@
           class="text-white transform active:scale-95 transition-all hover:bg-white hover:bg-opacity-10 duration-300 outline-none focus:ring-1 focus:ring-white group flex flex-col"
         >
           <svg
-            class="h-6 w-6 flex-1 m-5"
+            class="h-6 w-6 m-5"
             viewBox="0 0 24 24"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
@@ -99,7 +99,7 @@
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 flex-1 m-5"
+            class="h-6 w-6 m-5"
             viewBox="0 0 24 24"
             fill="currentColor"
             ><path
@@ -116,7 +116,7 @@
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 flex-1 m-5"
+            class="h-6 w-6 m-5"
             viewBox="0 0 24 24"
             fill="currentColor"
             ><path
